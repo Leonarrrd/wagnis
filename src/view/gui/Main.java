ca6e97eca23e47a7f544ff00ba4972d169c2e8b9
Philipp Moritzer <philipp.moritzer@yahoo.de>
2019-05-29 12:29:00 +0200

    .

--- src/view/gui/Main.java
+++ src/view/gui/Main.java
@@ -3,7 +3,6 @@ package view.gui;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import view.gui.roots.GameBorderPane;
 import view.gui.roots.StartBorderPane;
 import view.gui.scenes.MainScene;
 
@@ -15,7 +14,7 @@ public class Main extends Application {
     public void start(Stage stage) {
         Scene startupScene = new MainScene(new StartBorderPane());
 //        Scene startupScene = new MainScene(new GameBorderPane());
-        stage.setTitle("Risiko - Leonard der Gestaltige & Assistent");
+        stage.setTitle("Risiko - Leonard der Gestaltige & Big Assistent");
         stage.setScene(startupScene);
         stage.show();
     }
